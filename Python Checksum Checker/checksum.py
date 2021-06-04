968aff5bdf22fe74ad88aac54fac879e68ba0429
Juan <jcorlandomc@yahoo.com>
2021-06-03 22:13:51 -0700

    06/03/2021 10:13 pm

--- Python Checksum Checker/checksum.py
+++ Python Checksum Checker/checksum.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 from tkinter import *
 
+window = Tk()                                                    # Create root node
+window.geometry("500x350")                                          # Set intial height and width of window
+window.columnconfigure(0, weight=1)                                 # Configure First Column
+window.rowconfigure(0, weight=1)                                    # Configure First Row
+title = Label(window, text="Checksum Checker", fg="white", bg="black")   # Create the Title Label widget
+title.grid(row=0, column=0, sticky="new")                           # "new" means "north", "east", "west"
 
-window = Tk()                           # Create the root (base) window
-# set window size
-window.geometry("500x350")
-window.mainloop()                       # Start the event loop
 
 
 
+window.mainloop()
