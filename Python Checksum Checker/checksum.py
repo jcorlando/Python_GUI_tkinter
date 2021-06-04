220fe7ef48411ba114e54228ca1e93a70654d852
Juan <jcorlandomc@yahoo.com>
2021-06-04 16:30:52 -0700

    06/04/2021 04:30 pm

--- Python Checksum Checker/checksum.py
+++ Python Checksum Checker/checksum.py
@@ -6,9 +6,10 @@ window = Tk()                                       # Create root node
 window.title('File Checksum Hash Checker')
 
 # setting the minimum size of the root window
-window.minsize(630, 200)
+window.minsize(850, 200)
 
-window.geometry("650x350")    # Set intial height and width of window
+# Set intial height and width of window
+window.geometry("850x350")
 
 #----------Add a menubar with exit command--------------
 menubar = Menu(window)
@@ -33,17 +34,29 @@ labelButton = Label(window, text="Choose a file to find its Checksum:")
 labelButton.grid(row=1, column=0, sticky="nw", padx=10)
 
 #------------Define a Function to save the filepath--------------
-def filepathname():
-    window.filename = filedialog.askopenfilename(title="Choose a File")
+filepath = StringVar()
+filepath.set("")
+def openFile():
+    filepath.set(filedialog.askopenfilename(title="Choose a File"))
+    label_path = Label(window, textvariable=filepath)
+    label_path.grid(row=2, column=0, sticky="nw", padx=180)
+    filepath_string = filepath.get()
 #------------Define a Function to save the filepath--------------
 
-button1 = Button(window, text ="Choose filepath", command=filepathname)
+button1 = Button(window, text ="Choose filepath", command=openFile)
 button1.grid(row=1, column=0, sticky="nw", padx=250)
-
 
 window.rowconfigure(2, weight=1)                                            # Configure Row 2
 label_file_path = Label(window, text="The Path to your file is :")
 label_file_path.grid(row=2, column=0, sticky="nw", padx=10)
+
+
+
+
+
+
+
+
 
 
 
