56a416c395ab9dbdf9f0284e0c6f9bbd4b6819d0
Juan <jcorlandomc@yahoo.com>
2021-06-06 15:42:57 -0700

    06/06/2021 03:42 pm

--- Python Checksum Checker/checksum.py
+++ Python Checksum Checker/checksum.py
@@ -2,14 +2,15 @@
 from tkinter import *
 from tkinter import filedialog
 
+
 window = Tk()                                       # Create root node
 window.title('File Checksum Hash Checker')
 
 # setting the minimum size of the root window
-window.minsize(850, 200)
+window.minsize(850, 290)
 
 # Set intial height and width of window
-window.geometry("850x350")
+window.geometry("850x290")
 
 #----------Add a menubar with exit command--------------
 menubar = Menu(window)
@@ -33,31 +34,50 @@ title.grid(row=0, column=0, sticky="new")                                   # "n
 labelButton = Label(window, text="Choose a file to find its Checksum:")
 labelButton.grid(row=1, column=0, sticky="nw", padx=10)
 
-#------------Define a Function to save the filepath--------------
+#------------Define a Function For When Button is clicked and filepath is saved--------------
 filepath = StringVar()
 filepath.set("")
+md5_sum = StringVar()
+md5_sum.set("")
+md5_sum.set("Hello World!")
+sha1_sum = StringVar()
+sha1_sum.set("")
+sha1_sum.set("Hello World!")
+sha256_sum = StringVar()
+sha256_sum.set("")
+sha256_sum.set("Hello World!")
 def openFile():
     filepath.set(filedialog.askopenfilename(title="Choose a File"))
     label_path = Label(window, textvariable=filepath)
     label_path.grid(row=2, column=0, sticky="nw", padx=180)
     filepath_string = filepath.get()
-#------------Define a Function to save the filepath--------------
+    label_md5_sum = Label(window, textvariable=md5_sum)
+    label_md5_sum.grid(row=3, column=0, sticky="nw", pady=20, padx=180)
+    label_sha1_sum = Label(window, textvariable=sha1_sum)
+    label_sha1_sum.grid(row=4, column=0, sticky="nw", pady=20, padx=180)
+    label_sha256_sum = Label(window, textvariable=sha256_sum)
+    label_sha256_sum.grid(row=5, column=0, sticky="nw", pady=20, padx=180)
+#------------Define a Function For When Button is clicked and filepath is saved--------------
 
 button1 = Button(window, text ="Choose filepath", command=openFile)
 button1.grid(row=1, column=0, sticky="nw", padx=250)
 
-window.rowconfigure(2, weight=1)                                            # Configure Row 2
+
 label_file_path = Label(window, text="The Path to your file is :")
 label_file_path.grid(row=2, column=0, sticky="nw", padx=10)
 
 
+label_MD5_Sum = Label(window, text="The MD5 Sum is :")
+label_MD5_Sum.grid(row=3, column=0, sticky="nw", pady=20, padx=10)
 
 
+label_MD5_Sum = Label(window, text="The SHA1 Sum is :")
+label_MD5_Sum.grid(row=4, column=0, sticky="nw", pady=20, padx=10)
 
 
-
-
-
+window.rowconfigure(5, weight=1)                          # Configure Row 5
+label_MD5_Sum = Label(window, text="The SHA256 Sum is :")
+label_MD5_Sum.grid(row=5, column=0, sticky="nw", pady=20, padx=10)
 
 
 window.config(menu=menubar)
